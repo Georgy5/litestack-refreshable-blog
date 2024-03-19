213ad6918164be54e1bd8dca9a33e25fd1f69b80
Karl Keller <kgk86@hotmail.com>
2024-03-19 11:49:02 +0100

    Generate Blog scaffold and Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_19_104646) do
+  create_table "blogs", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.integer "blog_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_posts_on_blog_id"
+  end
+
+  add_foreign_key "posts", "blogs"
 end
